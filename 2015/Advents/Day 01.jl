871ada876d1694c1c3d39afa344c216f2cbd4471
Jean-Luc Parker <jldegp@gmail.com>
2022-10-29 22:22:45 +0100

    Day  01

--- 2015/Advents/Day 01.jl
+++ 2015/Advents/Day 01.jl
@@ -1,12 +1,22 @@
+# Functions
+function to_direction(instructions::String)::Vector{Int}
+    key = Dict("(" => 1, ")" => -1)
+    lookup_key(x) = key[string(x)]
+    instructions |> collect .|> lookup_key
+end
+function at_basement(directions::Vector{Int})::Int
+    position = 0
+    floor = 0
+    while floor >= 0
+        position += 1
+        floor += popfirst!(directions)
+    end
+    position
+end
+instructions = read("2015/Input/Day 01.txt", String)
 # Part 1
 ## Count the "("
-instructions = read("2015/Input/Day 01.txt", String)
-
-total = length(instructions)
-up = sum(string(x) == "(" for x in instructions)
-down = total - up
-
-floor = up - down
-print(floor)
+instructions |> to_direction |> sum
 
 # Part 2
+instructions |> to_direction |> at_basement
