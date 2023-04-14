a74c62a4bebf0384d5c466d86220b8547691b33e
Jean-Luc Parker <jldegp@gmail.com>
2023-04-14 21:21:20 +0100

    Day 02 Part 2

--- 2016/Advents/Day 02.jl
+++ 2016/Advents/Day 02.jl
@@ -1,54 +1,67 @@
 # Part 1
-
-instructions = readlines("Inputs/Day 02.txt")
+## Define functions up(), down(), left(), right(), to move the users finger
+## Use move() to reduce a String of instructions to the final digit, starting at 5
+## Use accumulate() to reduce a Vector{String} to the final digit whilst recording each digit and passing to the next set of instructions 
+read_ins() = readlines("Inputs/Day 02.txt")
 
 up(x::Integer) = if x > 3 x - 3 else x end
 down(x::Integer) = if x < 7 x + 3 else x end
 left(x::Integer) = if mod(x, 3) != 1 x - 1 else x end
 right(x::Integer) = if mod(x, 3) != 0 x + 1 else x end
 
-function move(init::Integer, instruction::String)
+function move(init::Integer, i::String)
     # Function to convert instruction character to function e.g., 'U' => up
     translate(direction) = Dict('U' => up, 'D' => down, 'L' => left, 'R' => right)[direction]
-    # Map translate() across Vector{Char} e.g., "RRLDU" => [right, right, left, down, up]
-    directions = map(translate, collect(instruction))
+    # Map translate() across Vector{Char} 
+    # E.g., collect("RRLDU") = ['R', 'R', 'L', 'D', 'U'] => [right, right, left, down, up]
+    directions = i |> collect .|> translate
     # Apply instructions e.g., init |> right |> right |> left |> down |> up
     reduce(|>, directions, init = init)
 end
 
-get_code(start::Integer, instructions::Vector{String}) = accumulate(move, instructions, init = start)
+get_code(i::Vector{String}) = accumulate(move, i, init = 5)
 
-get_code(5, ["ULL", "RRDDD", "LURDL", "UUUUD"]) # 1985
-
+## Illustration
+get_code(["ULL", "RRDDD", "LURDL", "UUUUD"]) # [1, 9, 8, 5]
 ## Application
+read_ins() |> get_code # [6, 5, 5, 5, 6]
 
-get_code(5, instructions) # [6, 5, 5, 5, 6]
+# Part 2
+## Similar procedure as Part 1, except the new numberpad changes boundary conditions & operations change
+## The 'A', 'B', 'C', 'D' are treated as 10:13 in move₂() to simplify calculation
+## decode() converts back into original numberpad key e.g., decode.([5, 10]) = [5, 'A']
+decode(n::Integer) = Dict(zip(1:13, vcat(1:9, ['A', 'B', 'C', 'D'])))[n]
 
-instruction = instructions[1]
+function up₂(x::Integer)
+    if x ∈ [3, 13]
+        x - 2
+    elseif x ∉ [5, 2, 1, 4, 9]
+        x - 4
+    else
+        x
+    end
+end
+function down₂(x::Integer)
+    if x ∈ [1, 11]
+        x + 2
+    elseif x ∉ [5, 10, 13, 12, 9]
+        x + 4
+    else
+        x
+    end
+end
+left₂(x::Integer) = if x ∉ [1, 2, 5, 10, 13] x - 1 else x end
+right₂(x::Integer) = if x ∉ [1, 4, 9, 12, 13] x + 1 else x end
 
-numberpad = transpose([1:3 4:6 7:9])
-
-
-# Old attemp at Part 1
-
-# LR and UD are orthogonal, their positions can be tracked separately
-# The solution is a plus scan over the array of characters where L, D => -1 and R, U => 1 respectively
-# The plus scan will give the net left / right, down / up moves
-# This net figure will be capped depending on the starting location e.g., at most ±1 starting at 5
-
-LR(instruction) = reduce(+, map(char -> Dict('L' => -1, 'R' => 1, 'D' => 0, 'U' => 0)[char], collect(instruction)))
-DU(instruction) = reduce(+, map(char -> Dict('L' => 0, 'R' => 0, 'D' => -1, 'U' => 1)[char], collect(instruction)))
-
-LRDU(instruction) = [DU(instruction), LR(instruction)]
-
-get_ind(n) = (Tuple ∘ first)(findall(==(n), numberpad))
-
-contract_range(ind) = if ind < 1 1 elseif ind > 3 3 else ind end
-
-function move(n, instruction)
-    ind₂ = (CartesianIndex ∘ Tuple)(contract_range.(get_ind(n) .+ LRDU(instruction)))
-    numberpad[ind₂]
+function move₂(init::Integer, i::String)
+    translate(direction) = Dict('U' => up₂, 'D' => down₂, 'L' => left₂, 'R' => right₂)[direction]
+    directions = i |> collect .|> translate
+    reduce(|>, directions, init = init)
 end
 
-# Need to change the plus scan. Give it an init? needs to shift between -init .+ [-1, 0, 1] where init ∈ -1:1
-move(5, "RRL")
+get_code₂(i::Vector{String}) = accumulate(move₂, i, init = 5)
+
+## Illustration
+["ULL", "RRDDD", "LURDL", "UUUUD"] |> get_code₂ .|> decode # [5, 'D', 'B', 3]
+## Application
+read_ins() |> get_code₂ .|> decode # ['C', 'B', 7, 7, 9]
