b8eaaea73c8b80062beaa050f8b0ec4890955075
Jean-Luc Parker <jldegp@gmail.com>
2023-07-29 21:34:16 +0100

    2016 04 Pt02

--- 2016/Advents/Day 04.jl
+++ 2016/Advents/Day 04.jl
@@ -1,5 +1,4 @@
 using DataFrames
-
 # Part 1
 remove_last(v::Vector) = v[1:end-1]
 split_dash(str::String) = String.(split(str, "-"))
@@ -19,7 +18,30 @@ get_numeric(string::String) = parse(Int64, filter(isnumeric, string))
 is_valid_room(room::String) = get_checksum(room) == get_checksum_provided(room)
 count_valid_id(room_list::Vector{String}) = sum(get_numeric.(room_list)[is_valid_room.(room_list)])
 
-
 room_list = readlines("Inputs/Day 04.txt")
 count_valid_id(room_list) # 278221
 
+## Part 2
+get_encrypted_name_full(room) = room |> split_dash |> remove_last |> x -> join(x, "-")
+function rotate(char::Char, n::Integer)
+    num_to_char = Dict(1:26 .=> 'a':'z')
+    char_to_num = Dict('a':'z' .=> 1:26)
+    if char == '-'
+        ' '
+    else
+        num = char_to_num[char]
+        num = mod(num + n, 26)
+        if num == 0 num = 25 end
+        num_to_char[num]
+    end
+end
+decrypt(room_encrypted::String) = room_encrypted |> collect .|> rotate |> join
+function get_real_name(room)
+    sector_id = get_numeric(room)
+    rotate_n(char::Char) = rotate(char, sector_id)
+    room |> get_encrypted_name_full |> collect .|> rotate_n |> join    
+end
+
+sector_ids = get_numeric.(room_list)
+
+sector_ids[get_real_name.(room_list).== "northpole object storage"]
