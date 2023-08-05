f08d775ccffccb7b625f3d8ff77f8610597d90aa
Jean-Luc Parker <jldegp@gmail.com>
2023-08-05 20:14:49 +0100

    2016 06 Pt2

--- 2016/Advents/Day 06.jl
+++ 2016/Advents/Day 06.jl
@@ -1,10 +1,6 @@
 # Part 1
 using SplitApplyCombine
 using StatsBase
-
-signals = readlines("Inputs/Day 06.txt")
-
-
 function most_frequent(v::Vector)
     counts = countmap(v)
     unique_values = keys(counts)
@@ -13,9 +9,20 @@ function most_frequent(v::Vector)
     invert_counts = Dict(frequencies .=> unique_values)
     invert_counts[max_frequency]
 end
+correct_error_max(signals) = signals .|> collect |> invert .|> most_frequent |> join
 
-correct_error(signals) = signals .|> collect |> invert .|> most_frequent |> join
-
-correct_error(signals) # mlncjgdg
+signals = readlines("Inputs/Day 06.txt")
+correct_error_max(signals) # mlncjgdg
 
 # Part 2
+function least_frequent(v::Vector)
+    counts = countmap(v)
+    unique_values = keys(counts)
+    frequencies = values(counts)
+    min_frequency = minimum(frequencies)
+    invert_counts = Dict(frequencies .=> unique_values)
+    invert_counts[min_frequency]
+end
+correct_error_min(signals) = signals .|> collect |> invert .|> least_frequent |> join
+
+correct_error_min(signals) # bipjaytb
